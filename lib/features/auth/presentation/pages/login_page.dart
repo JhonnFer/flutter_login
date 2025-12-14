94635b710f416620a2cf6c359c1b9500e99c4611
JhonnFer <jhoncasq23@gmail.com>
2025-12-14 18:55:45 -0500

    web 5

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -6,7 +6,6 @@ import '../bloc/auth_event.dart';
 import '../bloc/auth_state.dart';
 import '../widgets/auth_text_field.dart';
 import '../widgets/auth_button.dart';
-import 'forgot_password_page.dart';
 import 'home_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -106,7 +105,7 @@ class _LoginPageState extends State<LoginPage> {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                    builder: (_) => const ForgotPasswordPage(),
+                                    builder: (_) => const HomePage(),
                                   ),
                                 );
                               },
